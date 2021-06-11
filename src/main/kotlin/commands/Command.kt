066befb975751dd269bc9d0b1e49c244e81da94f
tuhnik <huntlarsen@gmail.com>
2021-06-11 04:17:32 +0300

    move command

--- src/main/kotlin/commands/Command.kt
+++ src/main/kotlin/commands/Command.kt
@@ -1,5 +1,6 @@
+package commands
 import dev.kord.core.event.message.MessageCreateEvent
 
-abstract class Command(val name: String) {
+abstract class Command (val name: String) {
     abstract suspend fun execute(event: MessageCreateEvent)
 }
